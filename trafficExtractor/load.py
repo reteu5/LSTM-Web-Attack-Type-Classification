f6a855ada35c8215ee5efdc8fd1416ea8a078536
reteu <apeach5@kakao.com>
2023-06-08 16:39:12 +0900

    Fixed traffic exractor(single) + Add dataset

--- trafficExtractor/load.py
+++ trafficExtractor/load.py
@@ -2,20 +2,27 @@ from subprocess import Popen, PIPE
 
 def trim_tshark(tshark_data):
     decoded_string = tshark_data.decode()
-    # 비어있는 줄이 있다면 해당 줄은 건너뛰는 코드 추가
+    
+    # 비어있는 줄이 있다면 해당 줄은 건너뛰는 코드
+    # solePackets = decoded_string.split('\n')[:-1]
 
-    solePackets = decoded_string.split('\n')[:-1]
+    # 비어있는 줄과 무관하게 context 살아있는 버전
+    solePackets = [line for line in decoded_string.split('\n') if line]
     return solePackets
 
 def open_tshark (pcap = None):
     # get HTTP GET method URI queries
-    tshark_output = Popen(['tshark', '-r', pcap, '-T', 'fields', '-e', 'http.request.uri.query'], stdout=PIPE)
-    # Save every traffic from a single pcap file
-    wild_output = tshark_output.stdout.read()
-    # Save as a list format / line by line
-    packetlist = trim_tshark(wild_output)
-    
-    return packetlist
+    try:
+        tshark_output = Popen(['tshark', '-r', pcap, '-T', 'fields', '-e', 'http.request.uri.query'], stdout=PIPE)
+        # Save every traffic from a single pcap file
+        wild_output = tshark_output.stdout.read()
+        # Save as a list format / line by line
+        packetlist = trim_tshark(wild_output)
+        return packetlist
+    except FileNotFoundError:
+        print("[-] tshark command not found.")
+    except Exception as e:
+        print(f"[-] An error occured : {e}")
 
 
 # tshark -r <pcap> -T fields -e http.request.uri.query
