d5c77ffdbcd778f71810b038533ba74c86ca0de3
reteu <apeach5@kakao.com>
2023-06-08 17:47:20 +0900

    Added loadtxt

--- trafficExtractor/loadtxt.py
+++ trafficExtractor/loadtxt.py
@@ -1,3 +1,11 @@
+
+def main() :
+    file = open("../dataset/xss_1484.txt", 'r')
+    lines = file.readlines()
+    # debug
+    print(lines)
+    file.close()
+
+
 if __name__ == "__main__" :
     main()
-    
