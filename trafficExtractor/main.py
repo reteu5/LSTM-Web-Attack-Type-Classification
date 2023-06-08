0d58d1e067c07af3612240890b82d4f9a501d8c1
reteu <apeach5@kakao.com>
2023-06-08 15:24:31 +0900

    Load file w. tshark display filter

---
+++ trafficExtractor/main.py
@@ -0,0 +1,18 @@
+from load import open_tshark
+from sys import argv
+from os import listdir
+from trace import Trace
+import numpy as np
+
+
+def main():
+    # USAGE: python3 main.py <pcapFile>
+    if argv[1][-5:] != '.pcap':
+        print("[-] Please specify a pcap file.")
+        exit(1)
+    else:
+        pcapFileName = argv[1]
+    
+    # Get a list of packets from a pcap file
+    listofPackets = open_tshark(pcapFileName)
+    
